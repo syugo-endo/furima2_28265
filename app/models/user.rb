172b65b1ce18f83c0338d672f6a76becd4748d5d
syugo-endo <1999endou@gmail.com>
2020-08-07 18:43:56 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
   has_many :buys
+  
+  with_options presence: true do
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
+    validates :name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+    validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "は半角英数で入力してください。"}
+  end
 end
