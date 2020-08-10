3c527958c5bb52ba1879a74e6dbc89a4a9638cc7
syugo-endo <1999endou@gmail.com>
2020-08-10 17:48:46 +0900

    shipping_fee date ship_from プルダウン導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
+  # belongs_to_active_hash :category, :condition, :ship_from, :shipping_date, :shipping_fee
   validates :category, numericality: { other_than: 1 }
-  belongs_to_active_hash :condition
   validates :condition, numericality: { other_than: 1 }
+  validates :shipping_fee, numericality: { other_than: 0 }
+  validates :ship_from, numericality: { other_than: 1 }
+  validates :shipping_date, numericality: { other_than: 1 }
+
+  validates :category, :condition, :shipping_fee, :shipping_date, :ship_from, acceptance: true
+  validates :text, :name, presence: true
 
   belongs_to :user
   has_one :buy
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'users/index'
   get 'items/index'
+  post 'items/new'
   devise_for :users
   root "items#index"
   resources :users, only: [:new, :create, :destroy]
-  resources :items, only: [:new]
+  resources :items, only: [:new, :create]
 end
