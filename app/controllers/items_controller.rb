55ce408abb91b39b887f43140ee51afbbce8e7cc
syugo-endo <1999endou@gmail.com>
2020-08-21 12:13:27 +0900

    updateのコントローラーが作動するように

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,8 +25,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if item.update(item_params)
-      redirect_to item_path(item)
+    if @item.update(item_params)
+      redirect_to item_path(@item)
     else
       render :edit
     end
